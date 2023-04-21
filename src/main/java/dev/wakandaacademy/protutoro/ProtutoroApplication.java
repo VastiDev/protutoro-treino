7ab3c41037cc2a43d1ffb995f65a386622061502
vastidev <Vastiane1@gmail.com>
2023-04-21 17:06:26 -0300

    init commit

---
+++ src/main/java/dev/wakandaacademy/protutoro/ProtutoroApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.protutoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtutoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtutoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/protutoro/ProtutoroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.protutoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtutoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
